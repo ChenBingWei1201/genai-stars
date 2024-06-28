cac0ab4d5c662b30a16ecbd029c4c7635396d73e
ChenBingWei1201 <rain4030633@gmail.com>
2024-06-28 14:20:40 +0800

    add constsnt

---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+// base url
+export const BASE_URL = "https://api.twelvelabs.io/v1.2";
+
+// headers
+export const HEADERS = {
+  application: "application/json",
+  "Content-Type": "application/json",
+  "x-api-key": process.env.TWELVE_LABS_API_KEY,
+};
+
+// engines
+export const ENGINES = [
+  {
+    name: "marengo2.6",
+    options: ["visual", "conversation", "text_in_video", "logo"],
+  },
+  {
+    name: "pegasus1",
+    options: ["visual", "conversation"],
+  },
+];
