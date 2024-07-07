006b9d28877645f4710ba6959d546a1f3bcf01c9
ChenBingWei1201 <rain4030633@gmail.com>
2024-07-07 21:30:04 +0800

    add web analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import ConvexClerkProvider from "../providers/ConvexClerkProvider";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -21,7 +23,11 @@ export default function RootLayout({
   return (
     <ConvexClerkProvider>
       <html lang="en">
-        <body className={manrope.className}>{children}</body>
+        <body className={manrope.className}>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+          </body>
       </html>
     </ConvexClerkProvider>
   );
